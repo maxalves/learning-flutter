b43220494c5c2bb4e313d6d24b78b1b3ba3b5bd9
Maxmiller Silva <maxmiller.silva@b2wdigital.com>
2020-11-15 01:10:35 -0300

    feature: add reset button, score and refactor app

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/result.dart';
 
-import './question.dart';
+import './quiz.dart';
 
 void main() => runApp(MyCoolApp());
 
@@ -12,46 +13,69 @@ class MyCoolApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyCoolApp> {
-  var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 0},
+        {'text': 'Green', 'score': 0},
+        {'text': 'Pink', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Lion', 'score': 0},
+        {'text': 'Duck', 'score': 0},
+        {'text': 'Isabelle', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'Who is your favorite instructor?',
+      'answers': [
+        {'text': 'Isa', 'score': 10},
+        {'text': 'Max', 'score': 0},
+        {'text': 'Marcia', 'score': 0},
+        {'text': 'Alan', 'score': 0},
+      ],
+    }
+  ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
 
+  void _resetQuiz(){
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Best App You Have Ever Seen'),
+          title: Text('Best game ever'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex],
-            ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () => print('Answer 3 chosen'),
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
